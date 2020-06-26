923cce131bc7cbffd51fbcfbbe29db2a4f458f08
adzamkomladev <adzamkomla.dev@gmail.com>
2020-06-26 08:46:51 +0000

    add top container on landing page

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
 import { LandingComponent } from './landing.component';
 
 @NgModule({
@@ -9,6 +13,9 @@ import { LandingComponent } from './landing.component';
   imports: [
     CommonModule,
     RouterModule.forChild([{ path: '', component: LandingComponent }]),
+    NzLayoutModule,
+    NzButtonModule,
+    NzGridModule,
   ],
 })
 export class LandingModule {}
