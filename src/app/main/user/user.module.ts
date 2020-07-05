01d86f05d8f6c7e3022d4a4d42a7e600d40edbfd
adzamkomladev <adzamkomla.dev@gmail.com>
2020-07-05 02:36:17 +0000

    add first part of user page

--- src/app/main/user/user.module.ts
+++ src/app/main/user/user.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+
 import { UserComponent } from './user.component';
 
 @NgModule({
@@ -9,6 +14,10 @@ import { UserComponent } from './user.component';
   imports: [
     CommonModule,
     RouterModule.forChild([{ path: ':id', component: UserComponent }]),
+    NzPageHeaderModule,
+    NzIconModule,
+    NzGridModule,
+    NzAvatarModule,
   ],
 })
 export class UserModule {}
